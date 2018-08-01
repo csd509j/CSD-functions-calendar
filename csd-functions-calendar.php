3a0ed6b37f9e9e2003caea57c9cd402d2ed0bdb2
Josh Armentano <josh@abidewebdesign.com>
2018-08-01 10:46:20 -0700

    Calendar updates

--- csd-functions-calendar.php
+++ csd-functions-calendar.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: CSD Functions - Calendar
-Version: 1.0
+Version: 1.1
 Description: Custom Google calendar implementation for district websites
 Author: Josh Armentano
 Author URI: https://abidewebdesign.com
@@ -41,8 +41,8 @@ function render_calendar() {
 		};
 		
 		var allEventSources = [];
-		<?php if( have_rows('calendars') ): ?>
-			<?php while( have_rows('calendars') ): the_row(); ?>
+		<?php if( have_rows('calendars', 'options') ): ?>
+			<?php while( have_rows('calendars', 'options') ): the_row(); ?>
 				<?php $calendar_address = get_sub_field('calendar_address'); ?>
 				<?php $calendar_name = get_sub_field('calendar_name'); ?>
 				<?php $calendar_color = get_sub_field('calendar_color'); ?>
@@ -77,7 +77,7 @@ function render_calendar() {
 				right: 'month,listYear'
 			},
 			
-			defaultView: window.mobilecheck() ? "list" : "month",
+			defaultView: window.mobilecheck() ? "listMonth" : "month",
 			
 			displayEventTime: true, // show the time column in list view
 			
@@ -110,14 +110,70 @@ function render_calendar() {
 <?php
 }
 
+function render_list_view() { 
+?>
+<div id='calendar-list'></div>
+<script>
+	$(function() {
+		var allEventSources = [];
+		
+		<?php if( have_rows('calendars', 'options') ): ?>
+			<?php while( have_rows('calendars', 'options') ): the_row(); ?>
+				<?php $calendar_address = get_sub_field('calendar_address'); ?>
+				<?php $calendar_name = get_sub_field('calendar_name'); ?>
+				<?php $calendar_color = get_sub_field('calendar_color'); ?>
+				
+				allEventSources.push(
+				{
+					id: '<?php echo $calendar_name; ?>', 
+					googleCalendarId: '<?php echo $calendar_address; ?>', 
+					textColor: '<?php echo $calendar_color; ?>',
+					backgroundColor: '<?php echo $calendar_color; ?>',
+					borderColor: '<?php echo $calendar_color; ?>',
+				});
+
+			<?php endwhile; ?>
+		<?php endif; ?>	
+		
+		$('#calendar-list').fullCalendar({
+
+			defaultView: 'listDay',
+			
+			displayEventTime: true, // show the time column in list view
+			
+			googleCalendarApiKey: 'AIzaSyCtn4VYI0llZ2sEGiMgezxWyBDTVuKaHds',
+				
+			eventSources: allEventSources,
+
+			eventClick: function (event) {
+				// opens events in a popup window
+				window.open(event.url, '_blank', 'width=700,height=600');
+				return false;
+			},
+			eventRender: function(eventObj, el) {
+				if (eventObj.description === undefined) {
+					eventObj.description = "";
+				} 
+				$(el).popover({
+					title: eventObj.title,
+					content: eventObj.description,
+					trigger: 'hover',
+					placement: 'top',
+					container: 'body'
+				}); 
+			}
+		});
+	});
+</script>
+<?php
+}
+
 function calendar_enqueue_script() {
-	if (is_page_template('page-calendar.php')) {
-		wp_enqueue_style( 'fullcalendar.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css' ); 
-		wp_enqueue_style( 'fullcalendar-style.css', plugin_dir_url( __FILE__ ) . 'style.css' ); 
-		wp_enqueue_script( 'moment.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js', 'jquery', '', true );
-		wp_enqueue_script( 'popper.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', 'jquery', '', true );
-		wp_enqueue_script( 'full-calendar.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js', 'jquery', '', true );
-		wp_enqueue_script( 'gcal.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/gcal.min.js', 'jquery', '', true );
-	}
+	wp_enqueue_style( 'fullcalendar.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css' ); 
+	wp_enqueue_style( 'fullcalendar-style.css', plugin_dir_url( __FILE__ ) . 'style.css' ); 
+	wp_enqueue_script( 'moment.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js', 'jquery', '', true );
+	wp_enqueue_script( 'popper.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', 'jquery', '', true );
+	wp_enqueue_script( 'full-calendar.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js', 'jquery', '', true );
+	wp_enqueue_script( 'gcal.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/gcal.min.js', 'jquery', '', true );
 }
 add_action( 'wp_enqueue_scripts', 'calendar_enqueue_script' );
